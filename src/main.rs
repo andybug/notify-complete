6ccd0a920306f351026f717ffaeaff7cbd80120d
Andrew Fields <andy@andybug.com>
2022-01-28 12:16:44 -0600

    Simplify main by moving to functions
    - Add function for updating the config with arg values
    - Add function for sending notifications

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod config;
 use clap::{AppSettings, Parser, ValueHint};
 use humantime::format_duration;
 use notify_rust::Notification;
-use std::process::Command;
+use std::process::{Command, ExitStatus};
 use std::time::{Duration, Instant};
 
 // Runs a command and sends a notification upon completion
@@ -18,37 +18,51 @@ struct Args {
     )]
     profile: String,
 
-    #[clap(short, long, default_value = "", help = "Title of the notification.")]
-    summary: String,
+    #[clap(short, long, help = "Title of the notification.")]
+    summary: Option<String>,
 
-    #[clap(short, long, default_value = "", help = "Notification contents.")]
-    body: String,
+    #[clap(short, long, help = "Notification contents.")]
+    body: Option<String>,
 
     #[clap(required = true, multiple_values = true, value_hint = ValueHint::CommandWithArguments, name = "cmd-with-args")]
     cmd: Vec<String>,
 }
 
+fn update_conf_from_args(conf: &mut config::Config, args: &Args) {
+    if args.summary.is_some() {
+        conf.profile.summary = String::from(args.summary.as_ref().unwrap());
+    }
+
+    if args.body.is_some() {
+        conf.profile.body = String::from(args.body.as_ref().unwrap());
+    }
+}
+
+fn send_notification(conf: &config::Config, duration: Duration, status: ExitStatus) {
+    let duration_str = format_duration(duration).to_string();
+
+    let mut body = String::from(conf.profile.body.as_str());
+    body.push('\n');
+    body.push_str(&format!("Result: {}\n", status.code().unwrap()));
+    body.push_str(&format!("Completed in {}", duration_str));
+
+    let result = Notification::new()
+        .summary(conf.profile.summary.as_str())
+        .body(body.as_str())
+        .appname("notify-complete")
+        .show();
+
+    match result {
+        Ok(handle) => println!("id = {}", handle.id()),
+        Err(e) => println!("{}", e),
+    }
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let conf = config::get_config();
     let args = Args::parse();
+    let mut conf = config::get_config(args.profile.as_str());
 
-    let profile = match conf.profiles.get(&args.profile) {
-        Some(p) => p,
-        None => {
-            eprintln!("Unknown profile '{}'. Using default profile.", args.profile);
-            conf.profiles.get(&"default".to_string()).unwrap()
-        }
-    };
-
-    let summary = match args.summary.len() > 0 {
-        true => args.summary,
-        false => String::from(&profile.summary),
-    };
-
-    let mut body = match args.body.len() > 0 {
-        true => args.body,
-        false => String::from(&profile.body),
-    };
+    update_conf_from_args(&mut conf, &args);
 
     let start = Instant::now();
 
@@ -58,30 +72,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .expect("Error creating child process");
 
     let child_result = child.wait().expect("Error waiting on child process");
-
     // using as_secs here to reduce the precision for humantime
     let elapsed_sec = Duration::from_secs((Instant::now() - start).as_secs());
 
-    let success_str = match child_result.success() {
-        true => "successfully completed",
-        false => "errored",
-    };
-
-    let duration_str = format_duration(elapsed_sec).to_string();
-
-    body.push('\n');
-    body.push_str(&format!("Command {} in {}", success_str, duration_str));
-
-    let result = Notification::new()
-        .summary(&summary)
-        .body(&body)
-        .appname("notify-complete")
-        .show();
-
-    match result {
-        Ok(handle) => println!("id = {}", handle.id()),
-        Err(e) => println!("{}", e),
-    }
+    send_notification(&conf, elapsed_sec, child_result);
 
     Ok(())
 }
